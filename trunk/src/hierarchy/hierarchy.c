2551461d592b922882dbe6ae908bb95bfe172aa8
cheetah.reasoner@gmail.com <cheetah.reasoner@gmail.com@cd4b721a-cceb-eb9d-7ddc-3d28a0bce084>
2014-03-06 13:14:04 +0000

    bugfix in computing hierarchy

--- trunk/src/hierarchy/hierarchy.c
+++ trunk/src/hierarchy/hierarchy.c
@@ -17,6 +17,8 @@
  */
 
 
+#include <stdlib.h>
+#include <assert.h>
 #include <Judy.h>
 
 #include "../model/datatypes.h"
@@ -46,41 +48,31 @@ void compute_concept_hierarchy(TBox* tbox) {
 			else {
 				is_direct_subsumer = 1;
 				int k,l;
-				for (k = 0; k < tbox->atomic_concept_list[i]->description.atomic->direct_subsumer_count; ++k) {
+
+				// make a temporary copy of the direct_subsumer_list since we are going to iterate on it and
+				// simultaneously remove elements from it.
+				int original_direct_subsumer_count = tbox->atomic_concept_list[i]->description.atomic->direct_subsumer_count;
+				Concept** tmp = malloc(original_direct_subsumer_count * sizeof(Concept*));
+				assert(tmp != NULL);
+				memcpy(tmp, tbox->atomic_concept_list[i]->description.atomic->direct_subsumer_list, original_direct_subsumer_count * sizeof(Concept*));
+
+				for (k = 0; k < original_direct_subsumer_count; ++k) {
 					// check if tbox->atomic_concept_list[i]->subsumer_list[j] is a subsumer of the direct_subsumer
 					// if yes, then tbox->atomic_concept_list[i]->subsumer_list[j] is not a direct subsumer of tbox->atomic_concept_list[i]
-					if (IS_SUBSUMED_BY(tbox->atomic_concept_list[i]->description.atomic->direct_subsumer_list[k],
-							tbox->atomic_concept_list[i]->subsumer_list[j])) {
+					if (IS_SUBSUMED_BY(tmp[k], tbox->atomic_concept_list[i]->subsumer_list[j])) {
 						is_direct_subsumer = 0;
 						break;
 					}
 					// now check if direct_subsumer is a subsumer of tbox->atomic_concept_list[i]->subsumer_list[j]
 					// if yes, then direct_subsumer_index is not a direct subsumer of tbox->atomic_concept_list[i]
 					// remove it from the list of direct subsumers
-					if (IS_SUBSUMED_BY(tbox->atomic_concept_list[i]->subsumer_list[j],
-							tbox->atomic_concept_list[i]->description.atomic->direct_subsumer_list[k]))
-						remove_direct_subsumer(tbox->atomic_concept_list[i]->description.atomic->direct_subsumer_list[k],
+					if (IS_SUBSUMED_BY(tbox->atomic_concept_list[i]->subsumer_list[j], tmp[k]))
+						remove_direct_subsumer(tmp[k],
 								tbox->atomic_concept_list[i]);
 				}
-				/*
-				direct_subsumer_index = 0;
-				J1F(is_direct_subsumer_list_nonempty, tbox->atomic_concept_list[i]->description.atomic->direct_subsumers, direct_subsumer_index);
-				while (is_direct_subsumer_list_nonempty) {
-					// check if tbox->atomic_concept_list[i]->subsumer_list[j] is a subsumer of the direct_subsumer_index
-					// if yes, then tbox->atomic_concept_list[i]->subsumer_list[j] is not a direct subsumer of tbox->atomic_concept_list[i]
-					if (IS_SUBSUMED_BY(((Concept*) direct_subsumer_index), tbox->atomic_concept_list[i]->subsumer_list[j])) {
-						is_direct_subsumer = 0;
-						break;
-					}
-					// now check if direct_subsumer_index is a subsumer of tbox->atomic_concept_list[i]->subsumer_list[j]
-					// if yes, then direct_subsumer_index is not a direct subsumer of tbox->atomic_concept_list[i]
-					// remove it from the list of direct subsumers
-					if (IS_SUBSUMED_BY(tbox->atomic_concept_list[i]->subsumer_list[j], ((Concept*) direct_subsumer_index)))
-						REMOVE_DIRECT_SUBSUMER(((Concept*) direct_subsumer_index), tbox->atomic_concept_list[i]);
+				// free the temporary copy
+				free(tmp);
 
-					J1N(is_direct_subsumer_list_nonempty, tbox->atomic_concept_list[i]->description.atomic->direct_subsumers, direct_subsumer_index);
-				}
-				*/
 				if (is_direct_subsumer)
 					add_direct_subsumer(tbox->atomic_concept_list[i]->subsumer_list[j], tbox->atomic_concept_list[i]);
 			}
