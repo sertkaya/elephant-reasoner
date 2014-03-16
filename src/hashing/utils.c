41a3d594db936dc16eb9e4dd949798c3dba23213
cheetah.reasoner@gmail.com <cheetah.reasoner@gmail.com@cd4b721a-cceb-eb9d-7ddc-3d28a0bce084>
2014-03-16 19:58:37 +0000

    further implementation of key_value_hash_table

--- src/hashing/utils.c
+++ src/hashing/utils.c
@@ -1,10 +1,7 @@
-/*
- * utils.c
- *
- *  Created on: 07.02.2014
- */
-
 #include <stdint.h>
+
+#include "key_hash_table.h"
+#include "key_value_hash_table.h"
 
 /* Round up to the next highest power of 2 */
 /* from http://www-graphics.stanford.edu/~seander/bithacks.html */
@@ -18,4 +15,20 @@ inline uint32_t roundup_pow2(uint32_t value) {
   ++value;
 
   return value;
+}
+
+inline int hash_unsigned(KeyHashTable* hash_table, uint32_t key) {
+	// return ((key << 5) - key) % hash_table->size;
+	// this is more efficient
+	return ((key << 5) - key) & hash_table->bucket_mask;
+}
+
+inline int hash_string(KeyValueHashTable* hash_table, unsigned char* key) {
+	unsigned long hash_value = 5381;
+	int c;
+
+	while (c = *key++)
+		hash_value = ((hash_value << 5) + hash_value) + c; /* hash * 33 + c */
+
+	return hash_value & hash_table->bucket_mask;
 }
