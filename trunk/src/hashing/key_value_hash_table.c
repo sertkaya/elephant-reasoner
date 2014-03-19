d3c09f4815d9a618a61d22c6cbd93e3e1c026192
cheetah.reasoner@gmail.com <cheetah.reasoner@gmail.com@cd4b721a-cceb-eb9d-7ddc-3d28a0bce084>
2014-03-19 15:46:49 +0000

    Improvement in key_value_hash_table: The stored key is now the hash value.

--- trunk/src/hashing/key_value_hash_table.c
+++ trunk/src/hashing/key_value_hash_table.c
@@ -17,24 +17,20 @@ inline KeyValueHashTable* create_key_value_hash_table(unsigned size) {
 	else
 		size = roundup_pow2(size);
 
-	hash_table->buckets = (Node***) malloc(size * sizeof(Node**));
+	// allocate space for the buckets
+	hash_table->buckets = (Node***) calloc(size, sizeof(Node**));
 	assert(hash_table->buckets != NULL);
 
-	hash_table->chain_sizes = (unsigned*) malloc(size * sizeof(unsigned));
+	// allocate space for the chain sizes
+	hash_table->chain_sizes = (unsigned*) calloc(size, sizeof(unsigned));
 	assert(hash_table->buckets != NULL);
 
 	hash_table->bucket_count = size;
-	hash_table->bucket_mask = hash_table->bucket_count - 1;
 
-	/*
-	int i;
-	for (i = 0; i < size; i++) {
-		hash_table->buckets[i] = NULL;
-		hash_table->chain_sizes[i] = 0;
-	}
-	*/
-	memset(hash_table->buckets, 0, hash_table->bucket_count * sizeof(Node**));
-	memset(hash_table->chain_sizes, 0, hash_table->bucket_count * sizeof(unsigned));
+	// memset(hash_table->buckets, 0, hash_table->bucket_count * sizeof(Node**));
+	// memset(hash_table->chain_sizes, 0, hash_table->bucket_count * sizeof(unsigned));
+
+	hash_table->tail = NULL;
 
 	return hash_table;
 }
@@ -48,7 +44,7 @@ int free_key_value_hash_table(KeyValueHashTable* hash_table) {
 			int j;
 			for (j = 0; j < hash_table->chain_sizes[i]; ++j) {
 				// note that we only free the node.
-				// the space allocated for node->key and node->value are not freed!
+				// the space allocated for  node->value is not freed!
 				free(hash_table->buckets[i][j]);
 				freed_bytes += sizeof(Node);
 			}
@@ -69,7 +65,7 @@ int free_key_value_hash_table(KeyValueHashTable* hash_table) {
 	return freed_bytes;
 }
 
-
+/*
 inline char insert_key_value(KeyValueHashTable* hash_table,
 		uint32_t hash_value,
 		int (*compare_func)(void* key_to_insert, void* key),
@@ -80,7 +76,7 @@ inline char insert_key_value(KeyValueHashTable* hash_table,
 
 	for (i = 0; i < chain_size; i++)
 		// if (hash_table->buckets[hash_value][i]->key == key)
-		if (!compare_func(key_to_insert, hash_table->buckets[hash_value][i]->key))
+		if (compare_func(key_to_insert, hash_table->buckets[hash_value][i]->key) == 0)
 			return 0;
 
 	Node** tmp = realloc(hash_table->buckets[hash_value], (chain_size + 1) * sizeof(Node*));
@@ -96,14 +92,69 @@ inline char insert_key_value(KeyValueHashTable* hash_table,
 
 	return 1;
 }
+*/
 
+
+inline char insert_key_value(KeyValueHashTable* hash_table,
+		uint32_t key,
+		void* value) {
+
+	int bucket_index = key & (hash_table->bucket_count - 1);
+	Node** bucket = hash_table->buckets[bucket_index];
+	int chain_size = hash_table->chain_sizes[bucket_index];
+
+	int i;
+	for (i = 0; i < chain_size; i++)
+		// if (hash_table->buckets[hash_value][i]->key == key)
+		if (key == bucket[i]->key)
+			return 0;
+
+	Node** tmp = realloc(bucket, (chain_size + 1) * sizeof(Node*));
+	assert(tmp != NULL);
+	bucket = hash_table->buckets[bucket_index] = tmp;
+
+	bucket[chain_size] = calloc(1, sizeof(Node));
+	assert(bucket[chain_size] != NULL);
+	bucket[chain_size]->key = key;
+	bucket[chain_size]->value = value;
+	bucket[chain_size]->previous = hash_table->tail;
+
+	hash_table->tail = bucket[chain_size];
+	++hash_table->chain_sizes[bucket_index];
+
+	return 1;
+}
+
+/*
 inline void* get_value(KeyValueHashTable* hash_table, uint32_t hash_value, int (*compare_func)(void* key_to_search, void* key), void* key_to_search) {
 	int chain_size = hash_table->chain_sizes[hash_value];
 
 	int i;
 	for (i = 0; i < chain_size; i++)
-		if (!compare_func(key_to_search, hash_table->buckets[hash_value][i]->key))
+		if (compare_func(key_to_search, hash_table->buckets[hash_value][i]->key) == 0)
 			return hash_table->buckets[hash_value][i]->value;
 
 	return NULL;
+}
+*/
+
+inline void* get_value(KeyValueHashTable* hash_table, uint32_t key) {
+	int bucket_index = key & (hash_table->bucket_count - 1);
+	Node** bucket = hash_table->buckets[bucket_index];
+	int chain_size = hash_table->chain_sizes[bucket_index];
+
+	int i;
+	for (i = 0; i < chain_size; i++)
+		if (key == bucket[i]->key)
+			return bucket[i]->value;
+
+	return NULL;
+}
+
+inline Node* last_node(KeyValueHashTable* hash_table) {
+	return hash_table->tail;
+}
+
+inline Node* previous_node(Node* current_node) {
+	return current_node->previous;
 }
--- trunk/src/hashing/key_value_hash_table.h
+++ trunk/src/hashing/key_value_hash_table.h
@@ -8,15 +8,18 @@ typedef struct node Node;
 typedef struct key_value_hash_table KeyValueHashTable;
 
 struct node {
-	void* key;
+	// void* key;
+	uint32_t key;
 	void* value;
+	Node* previous;
 };
 
 struct key_value_hash_table {
 	Node*** buckets;		// the buckets
-	unsigned bucket_mask;	// bit mask to access the buckets
 	unsigned bucket_count;	// the number of buckets
 	unsigned* chain_sizes;	// sizes of the chains
+	Node* tail;				// the last node of the hash.
+							// we maintain a backward linked list.
 };
 
 KeyValueHashTable* create_key_value_hash_table(unsigned size);
@@ -26,15 +29,39 @@ int free_key_value_hash_table(KeyValueHashTable* hash_table);
 /**
  * The compare_func returns 0 if the two keys are equal, 1 otherwise
  */
+/*
 inline char insert_key_value(KeyValueHashTable* hash_table,
 		uint32_t hash_value,
 		int (*compare_func)(void* key_to_insert, void* key),
 		void* key_to_insert,
 		void* value);
+		*/
+inline char insert_key_value(KeyValueHashTable* hash_table,
+		uint32_t key,
+		void* value);
 
+/*
 inline void* get_value(KeyValueHashTable* hash_table,
 		uint32_t hash_value,
 		int (*compare_func)(void* key_to_insert, void* key),
 		void* key);
+		*/
+
+inline void* get_value(KeyValueHashTable* hash_table,
+		uint32_t key);
+
+/**
+ * Returns the last node in the hash table or NULL if it is empty.
+ * Note that we iterate the nodes in hash table in the backward order
+ * of insertion, since the implementation is easier and the order of
+ * iteration is not relevant for our purposes.
+ */
+inline Node* last_node(KeyValueHashTable* hash_table);
+
+/**
+ * Returns the node that comes before the current node, or NULL if
+ * there is none.
+ */
+inline Node* previous_node(Node* current_node);
 
 #endif
