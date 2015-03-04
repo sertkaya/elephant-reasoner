f218a89a497c2c21a17ad60ae9b57a55e27803fd
ELepHant Reasoner <elephant-reasoner@gmail.com>
2015-03-04 15:01:01 +0100

    Added test for testing maps.

---
+++ src/test/test_map.c
@@ -0,0 +1,41 @@
+/*
+ * The ELepHant Reasoner
+ *
+ * Copyright (C) Baris Sertkaya (sertkaya.baris@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "../hashing/utils.h"
+#include "../utils/map.h"
+
+int main(int argc, char *argv[]) {
+	Map map;
+	MAP_INIT(&map, 16);
+
+	char* prefix_name = "";
+	char* prefix = "<http://www.w3.org/2002/07/owl#>";
+	MAP_PUT(hash_string(prefix_name), prefix, &map);
+
+	char str[32] = ":Thing";
+	char delim[2] = ":";
+	char* token = strtok(str, delim);
+	printf("token: %s\n", token);
+	printf("token value: %s\n", (char*) MAP_GET(hash_string(token), &map));
+
+	return 0;
+}
+
